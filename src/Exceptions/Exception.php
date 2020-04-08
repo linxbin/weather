fdb48472e8ec70a190571a2fd24f9b7428f83924
Linxb <448923515@qq.com>
2020-04-08 14:04:10 +0800

    first commit

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dark
+ * Date: 2020-04-06
+ * Time: 21:48
+ */
+
+namespace Linxb\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/HttpException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dark
+ * Date: 2020-04-06
+ * Time: 21:49
+ */
+
+namespace Linxb\Weather\Exceptions;
+
+class HttpException extends Exception
+{
+
+}
