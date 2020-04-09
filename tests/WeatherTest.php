375cdc816c043f268c6cc3115250dc77711a00b1
Lin Xiaobin <448923515@qq.com>
2020-04-09 02:30:18 +0000

    Apply fixes from StyleCI

--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -1,9 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dark
- * Date: 2020-04-07
- * Time: 09:56
+
+/*
+ * This file is part of the linxb/weather.
+ *
+ * (c) linxb <i@linxb.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Linxb\Weather\Tests;
